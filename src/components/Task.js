6bb0f55299eb9d343cc5ee8b2b62f9a4dfb9040d
Deepak Kumar <kumar.deepak@webseeder.in>
2024-07-19 13:24:39 +0530

    bug fixed in update components

--- src/components/Task.js
+++ src/components/Task.js
@@ -41,198 +41,202 @@ function TaskBoard({ tasks, onDelete, onEdit }) {
 }
 
 export default function Task() {
-    const [notice, setNotice] = useState(false);
-    const [showEdit, setShowEdit] = useState(false);
-    const [editId, setEditId] = useState('');
-    const [allTasks, setAllTasks] = useState([]);
-    const [filteredTasks, setFilteredTasks] = useState([]);
-    const [formData, setFormData] = useState({
-      title: '',
-      description: '',
-    });
-  
-    useEffect(() => {
-      getAllTasks();
-    }, []);
-  
-    const getAllTasks = async () => {
-      try {
-        const response = await axios.get(`${API_URL}/api/v1/task/getAllTasks`);
-        setAllTasks(response.data.tasks);
-        setFilteredTasks(response.data.tasks);
-      } catch (error) {
-        console.error(error);
-        alert('Failed to fetch tasks. Please try again.');
-      }
-    };
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    };
-  
-    const handleSubmit = async (e) => {
-      e.preventDefault();
-      try {
-        await axios.post(`${API_URL}/api/v1/task/createTask`, formData);
-        alert('Task created successfully!');
-        setNotice(false);
-        setFormData({ title: '', description: '' });
-        await getAllTasks();
-      } catch (error) {
-        console.error(error);
-        alert('Failed to create task. Please try again.');
-      }
-    };
-  
-    const handleDelete = async (id) => {
-      try {
-        await axios.delete(`${API_URL}/api/v1/task/deleteTaskById/${id}`);
-        alert('Task deleted successfully!');
-        await getAllTasks();
-      } catch (error) {
-        console.error(error);
-        alert('Failed to delete task. Please try again.');
-      }
-    };
-  
-    const handleEditSubmit = async () => {
-      try {
-        await axios.patch(`${API_URL}/api/v1/task/updateTaskById/${editId}`, formData);
-        alert('Task information updated successfully!');
-        setShowEdit(false);
-        await getAllTasks();
-      } catch (error) {
-        console.error(error);
-        alert('Failed to update task information. Please try again.');
-      }
-    };
-  
-    useEffect(() => {
-      const getData = async () => {
-        if (editId) {
-          const res = await axios.get(`${API_URL}/api/v1/task/getTaskById/${editId}`);
-          setFormData(res.data.task);
-          setShowEdit(true);
-        }
-      };
-      getData();
-    }, [editId]);
-  
-    const handleSearch = (e) => {
-      const searchQuery = e.target.value.toLowerCase();
-      const filtered = allTasks.filter((task) =>
-        task.title.toLowerCase().includes(searchQuery) ||
-        task.description.toLowerCase().includes(searchQuery)
-      );
-      setFilteredTasks(filtered);
-    };
-  
-    const categorizedTasks = {
-      todo: filteredTasks.filter(task => task.status === 'TODO'),
-      inProgress: filteredTasks.filter(task => task.status === 'IN_PROGRESS'),
-      done: filteredTasks.filter(task => task.status === 'DONE'),
-    };
+  const [notice, setNotice] = useState(false);
+  const [showEdit, setShowEdit] = useState(false);
+  const [editId, setEditId] = useState('');
+  const [allTasks, setAllTasks] = useState([]);
+  const [filteredTasks, setFilteredTasks] = useState([]);
+  const [formData, setFormData] = useState({
+    title: "",
+    description: "",
+  });
 
-    return (
-        <>
-        <Navbar/>
-            <div className="min-h-screen bg-gray-100">
-                <main className="p-4">
-                    <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4" onClick={() => setNotice(true)}>Add Task</button>
-                    <div className="bg-white p-4 shadow-md border rounded-md h-[60vh] overflow-x-auto">
-                        <div className="flex justify-between mb-4">
-                            <div>
-                                <label className="p-2">Search:</label>
-                                <input
-                                    type="text"
-                                    className="border p-1 rounded outline-blue-400"
-                                    placeholder="search..."
-                                    onChange={handleSearch}
-                                />
-                            </div>
-                        </div>
-                        <TaskBoard tasks={categorizedTasks} onDelete={handleDelete} onEdit={setEditId} />
-                    </div>
-                </main>
-                {notice && (
-                    <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-opacity-50 bg-gray-800">
-                        <div className="w-1/3 mx-auto p-8 bg-white shadow-lg rounded">
-                            <div className="font-bold text-xl mb-4">Add New Task</div>
-                            <form onSubmit={handleSubmit}>
-                                <div className="mb-4">
-                                    <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="title">Task Title</label>
-                                    <input
-                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
-                                        id="title"
-                                        type="text"
-                                        name="title"
-                                        value={formData.title}
-                                        onChange={handleChange}
-                                        placeholder="Enter Task Title"
-                                    />
-                                </div>
-                                <div className="mb-4">
-                                    <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="description">Task Description</label>
-                                    <input
-                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
-                                        id="description"
-                                        type="text"
-                                        name="description"
-                                        value={formData.description}
-                                        onChange={handleChange}
-                                        placeholder="Enter Task Description"
-                                    />
-                                </div>
-                                <div className="flex items-center justify-between">
-                                    <button onClick={() => setNotice(false)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Cancel</button>
-                                    <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Save</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                )}
-                {showEdit && (
-                    <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-opacity-50 bg-gray-800">
-                        <div className="w-1/3 mx-auto p-8 bg-white shadow-lg rounded">
-                            <div className="font-bold text-xl mb-4">Edit Task</div>
-                            <form onSubmit={handleEditSubmit}>
-                                <div className="mb-4">
-                                    <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="title">Task Title</label>
-                                    <input
-                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
-                                        id="title"
-                                        type="text"
-                                        name="title"
-                                        value={formData.title}
-                                        onChange={handleChange}
-                                        placeholder="Enter Task Title"
-                                    />
-                                </div>
-                                <div className="mb-4">
-                                    <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="description">Task Description</label>
-                                    <input
-                                        className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
-                                        id="description"
-                                        type="text"
-                                        name="description"
-                                        value={formData.description}
-                                        onChange={handleChange}
-                                        placeholder="Enter Task Description"
-                                    />
-                                </div>
-                                <div className="flex items-center justify-between">
-                                    <button onClick={() => setShowEdit(false)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Cancel</button>
-                                    <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Save</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                )}
+  useEffect(() => {
+    getAllTasks();
+  }, []);
+
+  const getAllTasks = async () => {
+    try {
+      const response = await axios.get(`${API_URL}/api/v1/task/getAllTasks`);
+      setAllTasks(response.data.tasks);
+      setFilteredTasks(response.data.tasks);
+    } catch (error) {
+      console.error(error);
+      alert('Failed to fetch tasks. Please try again.');
+    }
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post(`${API_URL}/api/v1/task/createTask`, formData);
+      alert('Task created successfully!');
+      setNotice(false);
+      setFormData({ title: '', description: '' });
+      await getAllTasks();
+    } catch (error) {
+      console.error(error);
+      alert('Failed to create task. Please try again.');
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete(`${API_URL}/api/v1/task/deleteTaskById/${id}`);
+      alert('Task deleted successfully!');
+      await getAllTasks();
+    } catch (error) {
+      console.error(error);
+      alert('Failed to delete task. Please try again.');
+    }
+  };
+
+  const handleEditSubmit = async (e) => {
+    console.log(editId, "editId");
+    console.log(formData, "formData");
+    e.preventDefault();
+    try {
+      await axios.patch(`${API_URL}/api/v1/task/updateTaskById/${editId}`, formData);
+      alert('Task information updated successfully!');
+      setShowEdit(false);
+      setEditId('');
+      await getAllTasks();
+    } catch (error) {
+      console.error(error);
+      alert('Failed to update task information. Please try again.');
+    }
+  };
+
+  useEffect(() => {
+    const getData = async () => {
+      if (editId) {
+        const res = await axios.get(`${API_URL}/api/v1/task/getTaskById/${editId}`);
+        setFormData(res.data.task);
+        setShowEdit(true);
+      }
+    };
+    getData();
+  }, [editId]);
+
+  const handleSearch = (e) => {
+    const searchQuery = e.target.value.toLowerCase();
+    const filtered = allTasks.filter((task) =>
+      task.title.toLowerCase().includes(searchQuery) ||
+      task.description.toLowerCase().includes(searchQuery)
+    );
+    setFilteredTasks(filtered);
+  };
+
+  const categorizedTasks = {
+    todo: filteredTasks.filter(task => task.status === 'TODO'),
+    inProgress: filteredTasks.filter(task => task.status === 'IN_PROGRESS'),
+    done: filteredTasks.filter(task => task.status === 'DONE'),
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="w-full bg-gray-100">
+        <main className="p-4">
+          <button className="bg-blue-500 text-white px-4 py-2 rounded mb-4" onClick={() => setNotice(true)}>Add Task</button>
+          <div className="bg-white p-4 shadow-md border rounded-md h-[60vh] overflow-x-auto">
+            <div className="flex justify-between mb-4">
+              <div>
+                <label className="p-2">Search:</label>
+                <input
+                  type="text"
+                  className="border p-1 rounded outline-blue-400"
+                  placeholder="search..."
+                  onChange={handleSearch}
+                />
+              </div>
             </div>
-        </>
-    )
+            <TaskBoard tasks={categorizedTasks} onDelete={handleDelete} onEdit={setEditId} />
+          </div>
+        </main>
+        {notice && (
+          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-opacity-50 bg-gray-800">
+            <div className="w-1/3 mx-auto p-8 bg-white shadow-lg rounded">
+              <div className="font-bold text-xl mb-4">Add New Task</div>
+              <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="title">Task Title</label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="title"
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    placeholder="Enter Task Title"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="description">Task Description</label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="description"
+                    type="text"
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                    placeholder="Enter Task Description"
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <button onClick={() => setNotice(false)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Cancel</button>
+                  <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Save</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+        {showEdit && (
+          <div className="flex justify-center items-center overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none bg-opacity-50 bg-gray-800">
+            <div className="w-1/3 mx-auto p-8 bg-white shadow-lg rounded">
+              <div className="font-bold text-xl mb-4">Edit Task</div>
+              <form onSubmit={handleEditSubmit}>
+                <div className="mb-4">
+                  <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="title">Task Title</label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="title"
+                    type="text"
+                    name="title"
+                    value={formData.title}
+                    onChange={handleChange}
+                    placeholder="Enter Task Title"
+                  />
+                </div>
+                <div className="mb-4">
+                  <label className="block text-zinc-700 text-sm font-bold mb-2" htmlFor="description">Task Description</label>
+                  <input
+                    className="shadow appearance-none border rounded w-full py-2 px-3 text-zinc-700 leading-tight focus:outline-none focus:shadow-outline"
+                    id="description"
+                    type="text"
+                    name="description"
+                    value={formData.description}
+                    onChange={handleChange}
+                    placeholder="Enter Task Description"
+                  />
+                </div>
+                <div className="flex items-center justify-between">
+                  <button onClick={() => setShowEdit(false)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">Cancel</button>
+                  <button type="submit" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">Save</button>
+                </div>
+              </form>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  )
 }
